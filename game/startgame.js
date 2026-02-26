33638cc23589585f06cedd8f0e5af2a792e5d4c9
vincenttrinh9098 <vincenttrinh9098@gmail.com>
2026-02-25 16:00:22 -0800

    added songs

--- game/startgame.js
+++ game/startgame.js
@@ -1,7 +1,33 @@
 const songs = [
 {file: "https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/2%20HELL%20&%20BACK.mp3", answer:"2 hell and back"},
 {file: "https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/2%20soon%20-%20keshi%20-%20Spotimate.app.mp3", answer:"2 soon"},
-{file: "https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/Say%20-%20keshi%20-%20Spotimate.app.mp3",answer: "say"}];
+{file: "https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/Say%20-%20keshi%20-%20Spotimate.app.mp3",answer: "say"},
+
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/ANGEL%20-%20keshi%20-%20Spotimate.app.mp3", answer:"angel"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/ANGOSTURA%20-%20keshi%20-%20Spotimate.app.mp3", answer:"angostura"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/B.Y.S.%20-%20keshi%20-%20Spotimate.app.mp3", answer:"bys"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/Bodies%20-%20keshi%20-%20Spotimate.app.mp3", answer:"bodies"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/Dream%20-%20keshi%20-%20Spotimate.app.mp3", answer:"dream"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/GET%20IT%20-%20keshi%20-%20Spotimate.app.mp3", answer:"get it"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/HELL_HEAVEN%20-%20keshi%20-%20Spotimate.app.mp3", answer:"hell/heaven"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/Id%20-%20keshi%20-%20Spotimate.app.mp3", answer:"id"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/Just%20To%20Die%20-%20keshi%20-%20Spotimate.app.mp3", answer:"just to die"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/Like%20That%20-%20keshi%20-%20Spotimate.app.mp3", answer:"like that"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/LIMBO%20-%20keshi%20-%20Spotimate.app.mp3", answer:"limbo"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/more%20-%20keshi%20-%20Spotimate.app.mp3", answer:"more"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/Night%20-%20keshi%20-%20Spotimate.app%20(1).mp3", answer:"night"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/Say%20-%20keshi%20-%20Spotimate.app.mp3", answer:"say"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/Soft%20Spot%20-%20keshi%20-%20Spotimate.app.mp3", answer:"soft spot"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/SOMEBODY%20-%20keshi%20-%20Spotimate.app.mp3", answer:"somebody"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/talk%20-%20keshi%20-%20Spotimate.app.mp3", answer:"talk"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/Texas%20-%20keshi%20-%20Spotimate.app.mp3", answer:"texas"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/TOUCH%20-%20keshi%20-%20Spotimate.app.mp3", answer:"touch"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/UNDERSTAND%20-%20keshi%20-%20Spotimate.app.mp3", answer:"understand"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/WANTCHU%20-%20keshi%20-%20Spotimate.app.mp3", answer:"wantchu"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/War%20-%20keshi%20-%20Spotimate.app.mp3", answer:"war"},
+{file:"https://hvgcbsycxjreanddnmaf.supabase.co/storage/v1/object/public/Keshi%20songs/WESTSIDE%20-%20keshi%20-%20Spotimate.app.mp3", answer:"westside"}
+
+];
 
 const submitBtn = document.getElementById("submit-btn");
 const userInput = document.getElementById("userInput");
